722ee77ce452d29445ec3cd400ae24fe891c445b
ruan <ruanlourencosilva.rj@gmail.com>
2024-12-18 14:44:47 -0300

    table and model created

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const db = require("./db/connection");
 
 const PORT = 3000;
 
@@ -7,7 +8,17 @@ app.listen( PORT , function() {
     console.log(`O express está rodando na porta ${PORT}`);
 });
 
+//db connection
+db.authenticate()
+.then( () => {
+       console.log("conectou ao banco com sucesso")
+})
+.catch( err => {
+    console.log("ocorreu um erro ao conectar", err);
+})
 
+
+// routes
 app.get("/", (req,res) =>{
     res.send("esta funcionando");
 }
