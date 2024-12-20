d392a80b9c31107af3722f10fbfd16a5917f43c0
ruan <ruanlourencosilva.rj@gmail.com>
2024-12-20 10:37:27 -0300

    search implemented

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const path       = require("path");
 const db         = require("./db/connection");
 const bodyParser = require("body-parser");
 const Job        = require("./models/Job")
-
+const Sequelize  = require("sequelize");
+const Op         = Sequelize.Op;
 
 const PORT = 3000;
 
@@ -37,14 +38,25 @@ db.authenticate()
 // routes
 app.get("/", (req,res) => {
 
+    let search = req.query.job;
+    let query = '%'+search+'%';
+
+    if(!search){
     Job.findAll({
         order: [["createdAt","DESC"]]})
         .then( jobs =>  {
             res.render("index",{jobs});
         })
-        .catch( err => console.log(err) )
+    } else {
+        Job.findAll({
+            where: {title: {[Op.like] : query}} ,
+            order: [["createdAt","DESC"]]})
+            .then( jobs =>  {
+                res.render("index",{jobs , search});
+            })
+    }
    
-})
+});
 
 //jobs routes
 app.use("/jobs" , require("./routes/jobs"))
